82f37a9bd9fc7f96874a44a35a2468c9610142d3
raghu8 <rvmanthena@gmail.com>
2022-03-12 11:37:02 -0500

    d

---
+++ src/main/java/com/parasyte/mediaCatalog/MediaCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.parasyte.mediaCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parasyte/mediaCatalog/MediaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parasyte.mediaCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
