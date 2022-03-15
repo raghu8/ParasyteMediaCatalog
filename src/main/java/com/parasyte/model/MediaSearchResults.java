1a215d7de94f0c695bd0e2104aa3c98b6de35f10
raghu8 <rvmanthena@gmail.com>
2022-03-14 23:39:27 -0400

    themoviedb api model

---
+++ src/main/java/com/parasyte/model/MediaSearchResults.java
@@ -0,0 +1,10 @@
+package com.parasyte.model;
+
+import java.util.ArrayList;
+
+public class MediaSearchResults {
+    public int page;
+    public ArrayList<MediaSearchResult> mediaSearchResults;
+    public int total_pages;
+    public int total_results;
+}
