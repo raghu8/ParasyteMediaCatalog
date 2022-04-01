735c26f7166b72cbfdd98484026840ca8441f202
raghu8 <rvmanthena@gmail.com>
2022-04-01 16:00:20 -0400

    movie search

--- src/main/java/com/parasyte/controller/ParasyteMediaDataController.java
+++ src/main/java/com/parasyte/controller/ParasyteMediaDataController.java
@@ -1,5 +1,31 @@
 package com.parasyte.controller;
 
-public class ParasyteMediaDataController {
+import com.parasyte.apiDao.MovieDbApiCall;
+import com.parasyte.model.MediaSearchResult;
+import com.parasyte.model.MediaSearchResults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
+@RestController
+public class ParasyteMediaDataController {
+    @Autowired
+    private MovieDbApiCall movieDbApiCall;
+
+    @Autowired
+    private MediaSearchResults mediaSearchResults;
+
+    //TODO: Create a util function that creates a list of different combinations of movie/show name (different combination of spaces)
+    @GetMapping("/hello")
+    public String testCall(){
+        return "Hello World!!";
+    }
+
+    @GetMapping("/movieDetails")
+    public MediaSearchResults movieDb(@RequestParam String movieName) throws IOException {
+        return movieDbApiCall.callData(movieName);
+    }
 }
--- src/main/java/com/parasyte/model/MediaSearchResults.java
+++ src/main/java/com/parasyte/model/MediaSearchResults.java
@@ -1,10 +1,45 @@
 package com.parasyte.model;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 
+@Service
 public class MediaSearchResults {
     public int page;
-    public ArrayList<MediaSearchResult> mediaSearchResults;
+    public ArrayList<MediaSearchResult> results;
     public int total_pages;
     public int total_results;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public ArrayList<MediaSearchResult> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<MediaSearchResult> results) {
+        this.results = results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
 }
